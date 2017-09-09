bab366a30f8ad69cd6ef83b9fa87f43f01f9bf49
Solomon Kingsley <kingsley.solomon@andela.com>
2017-09-09 02:53:46 +0100

    made few changes

--- server/app.js
+++ server/app.js
@@ -4,12 +4,22 @@ import webpack from 'webpack';
 import path from 'path';
 import http from 'http';
 import dotenv from 'dotenv';
+import BodyParser from 'body-parser';
 import log from 'log-with-colors';
 import webpackHotMidlleware from 'webpack-hot-middleware';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackConfig from '../webpack.config.dev';
-import BodyParser from 'body-parser';
 
+import UserRouter from './routes/UserRoutes';
+import QuestionRouter from './routes/QuestionRoutes';
+import AnswerRouter from './routes/AnswerRoutes';
+import VoteRouter from './routes/VotesRoutes';
+
+dotenv.load();
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
 const app = express(),
   compiler = webpack(webpackConfig);
 
@@ -28,14 +38,17 @@ app.use(BodyParser.urlencoded({
   extended: false
 }));
 
+app.use('/users', UserRouter);
+app.use('/questions', QuestionRouter);
+app.use('/answers', AnswerRouter);
+app.use('/votes', VoteRouter);
+
+app.use(logger('dev'));
+
 app.get('*', (req, res) => res.status(200)
 .sendFile(path.join(__dirname, '../client/index.html')));
 
-dotenv.load();
-const port = parseInt(process.env.PORT, 10) || 8000;
-app.set('port', port);
 
-const server = http.createServer(app);
 server.listen(port, (error) => {
   if (!error) {
     log.success(`listening to app ${port}`);
---
+++ server/routes/QuestionRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import QuestionController from '../controllers/QuestionController';
+
+const app = express.Router();
+
+//start logic
+
+//end logic
+export default app;
